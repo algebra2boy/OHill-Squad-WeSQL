54c2a059f1b46c66fd7d9ee1cd62147794028fef
Yongye <103079472+algebra2boy@users.noreply.github.com>
2022-08-23 17:25:47 -0400

    inserting done

--- main.py
+++ main.py
@@ -33,6 +33,40 @@ create table student(
 );
 
 """
+pop_living_area = """
+INSERT INTO living_area (campus_Area, building_name, Honor) VALUES
+("OHill", "Webster", 0),
+("OHill", "Grayson", 0),
+("OHill", "Field", 0),
+("Commonwealth", "Elm", 1),
+("Northeast", "Crabtree", 0),
+("Central", "Brook", 0)
+"""
+
+
+pop_Students = """
+INSERT INTO student (id, name, year, campus_Area, building_name) Values
+(1, "Yongye", 2, "OHill", "Webster"),
+(2, "Anan", 3, "Commonwealth", "Elm"),
+(3, "Jeff", 3, "OHill", "Grayson"),
+(4, "Matt", 3, "OHill", "Field"),
+(5, "Tina", 2, "Northeast", "Crabtree"),
+(6, "Adam", 3, "OHill", "Grayson"),
+(7, "Eric", 4, "Central", "Brook"),
+(8, "Navid", 4, "OHill", "Grayson")
+"""
+
+pop_Class = """
+INSERT INTO class (department, id, class_name, professor_name) VALUES
+("CS", 345 , "Practice and Applications of Data Management", "Jaime Davila"),
+("CS", 220 , "Programming Methologies", "Marius Minea "),
+("CS", 230 , "Computer Systems Principles", "Meng-Chieh Chiu"),
+("CS", 240 , "Reasoning under Uncertainty", "Andrew Lan"),
+("CS", 250 , "Introductio to Computation", "David Barrington"),
+("CS", 311, "Introduction to Algorithms", "Marius Minea"),
+("Math", 235, "Linear Algebra", "TBA"),
+("Math", 331, "Ordinary Differential Equations", "Garrett Cahill")
+"""
 
 
 def create_server_connection(host_name: str, user_name: str, user_password: str):
@@ -89,9 +123,18 @@ def main():
         print(err)
     # this connection is used to connect to the mySQL database/schema
     db = create_db_connection("127.0.0.1", "root", "root12ab", 'OHILL')
+
+    # creating the tables
     execute_query(db, create_living_area)
     execute_query(db, create_class)
-    execute_query(db,create_student)
+    execute_query(db, create_student)
+    
+    # inserting values into the tables 
+    execute_query(db, pop_living_area)
+    execute_query(db, pop_Students)
+    execute_query(db, pop_Class)
+
+    
 
 if __name__ == "__main__":
     main()
